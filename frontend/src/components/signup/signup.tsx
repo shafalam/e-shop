6a1423f30c838051dd495dae319a7dcf98b13876
shafalam <shafiulalam16@gmail.com>
2020-12-06 20:07:40 +0100

    added handler for input elements

--- frontend/src/components/signup/signup.tsx
+++ frontend/src/components/signup/signup.tsx
@@ -1,30 +1,104 @@
-import React, { FormEvent } from "react"
-import styles from "./signup.module.css"
+import React, { FormEvent } from "react";
+import styles from "./signup.module.css";
 
-
-const SignUp = () => {
-    const onFormSubmit = (e: FormEvent<HTMLElement>) => {
-        e.preventDefault()
-        
-        
-        console.log(e.target)
-    }
-
-
-    return(<div>
-        <form className={styles.form} onSubmit={onFormSubmit}>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="password" />
-            <select>
-                <option value="Debit Card">Debit Card</option>
-                <option value="Credit Card">Credit Card</option>
-                <option value="Bank transfer">Bank transfer</option>
-            </select>
-            <input type="number" placeholder="Balance" />
-            <input type="submit" className={styles.button} />
-        </form>
-    </div>)
+enum PaymentMethod {
+  "Debit Card",
+  "Credit Card",
+  "Bank Transfer",
 }
 
-export default SignUp
+const SignUp = () => {
+  const [formValue, setFormValue] = React.useState<{
+    name: string;
+    email: string;
+    password: string;
+    paymentMethod: PaymentMethod;
+    balance: number;
+  }>({
+    name: "",
+    email: "",
+    password: "",
+    paymentMethod: PaymentMethod["Debit Card"],
+    balance: 0,
+  });
+
+  const onFormSubmit = (e: FormEvent<HTMLElement>) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValue((prev) => ({ ...prev, name: e.target.value }));
+  };
+
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValue((prev) => ({ ...prev, email: e.target.value }));
+  };
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValue((prev) => ({ ...prev, password: e.target.value }));
+  };
+  const onChangePaymentMethod = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFormValue((prev) => ({
+      ...prev,
+      paymentMethod: (e.target.value as unknown) as PaymentMethod,
+    }));
+  };
+
+  const onChangeBalance = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValue((prev) => ({ ...prev, balance: +e.target.value }));
+  };
+
+  const options = [];
+  for (let method in PaymentMethod) {
+    if (isNaN(Number(method))) {
+      options.push(
+        <option value={method} key={method}>
+          {method}
+        </option>
+      );
+    }
+  }
+
+  return (
+    <div>
+      <form className={styles.form} onSubmit={onFormSubmit}>
+        <input
+          type="text"
+          value={formValue.name}
+          placeholder="Name"
+          onChange={onChangeName}
+        />
+        <input
+          type="email"
+          value={formValue.email}
+          placeholder="Email"
+          onChange={onChangeEmail}
+        />
+        <input
+          type="password"
+          value={formValue.password}
+          placeholder="password"
+          onChange={onChangePassword}
+        />
+        <label>
+          Payment Method
+          <select
+            value={formValue.paymentMethod}
+            onChange={onChangePaymentMethod}
+          >
+            {options}
+          </select>
+        </label>
+        <input
+          type="number"
+          value={formValue.balance}
+          placeholder="Balance"
+          onChange={onChangeBalance}
+        />
+        <input type="submit" className={styles.button} />
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
